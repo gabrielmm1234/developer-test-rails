84d696f3e9d7cb032746738aa66cee6585359aba
gabriel <gabrielmesquita@cjr.org.br>
2017-10-22 19:37:41 -0200

    Final test coverage for web pages controller.

--- estudologia/spec/controllers/web_pages_controller_spec.rb
+++ estudologia/spec/controllers/web_pages_controller_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe WebPagesController, type: :controller do
+  let(:valid_attributes) { {
+    title: "Estudologia",
+    description: "Factory para página web",
+    body: "Corpo de página web",
+    author: "Mesquita"
+  } }
+
+  let(:invalid_attributes) { { title: nil } }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -44,6 +53,81 @@ RSpec.describe WebPagesController, type: :controller do
       web_page = FactoryGirl.create :web_page
       get :edit, params: {id: web_page.to_param}
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+    it "creates a web page with valid attributes" do
+      expect {
+        post :create, params: {web_page: valid_attributes}
+      }.to change(WebPage, :count).by(1)
+    end
+
+    it "redirects to show page after creates the web page" do
+      post :create, params: {web_page: valid_attributes}
+      expect(response).to redirect_to(WebPage.last)
+    end
+
+    it "does not create a web page with invalid attributes" do
+      expect {
+        post :create, params: {web_page: invalid_attributes}
+      }.to change(WebPage, :count).by(0)
+    end
+
+    it "renders new page after not creating the web page" do
+      post :create, params: {web_page: invalid_attributes}
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "PUT #update" do
+    let(:new_attributes) { {
+      title: "Estudo",
+      description: "Factory para página web",
+      body: "Corpo de página web",
+      author: "Mesquita"
+    } }
+
+    it "updates a web page with valid attributes" do
+      web_page = WebPage.create! valid_attributes
+      put :update, params: {id: web_page.to_param, web_page: new_attributes}
+      web_page.reload
+      expect(web_page.title).to eq('Estudo')
+    end
+
+    it "redirects to show page after updating the web page" do
+      web_page = WebPage.create! valid_attributes
+      put :update, params: {id: web_page.to_param, web_page: new_attributes}
+      expect(response).to redirect_to(WebPage.last)
+    end
+
+    it "does not update a web page with invalid attributes" do
+      web_page = WebPage.create! valid_attributes
+      put :update, params: {id: web_page.to_param, web_page: invalid_attributes}
+      web_page.reload
+      expect(web_page.title).to eq('Estudologia')
+    end
+
+    it "renders edit page after not updating the web page" do
+      web_page = WebPage.create! valid_attributes
+      put :update, params: {id: web_page.to_param, web_page: invalid_attributes}
+      web_page.reload
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the web page" do
+      web_page = WebPage.create! valid_attributes
+      expect {
+        delete :destroy, params: {id: web_page.to_param}
+      }.to change(WebPage, :count).by(-1)
+    end
+
+    it "redirects to the index page" do
+      web_page = WebPage.create! valid_attributes
+      delete :destroy, params: {id: web_page.to_param}
+      expect(response).to redirect_to('/web_pages')
     end
   end
 end
