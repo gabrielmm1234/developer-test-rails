320c6433ad9248767384807760755718cedbaae5
gabriel <gabrielmesquita@cjr.org.br>
2017-10-22 18:57:47 -0200

    Added web page model validation.

--- estudologia/app/models/web_page.rb
+++ estudologia/app/models/web_page.rb
@@ -1,2 +1,6 @@
 class WebPage < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :body, presence: true
+  validates :author, presence: true
 end
