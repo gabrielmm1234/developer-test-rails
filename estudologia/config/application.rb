55391144303cc8523e463ef59f7a0f5d9df1ffd5
gabriel <gabrielmesquita@cjr.org.br>
2017-10-22 21:09:08 -0200

    Added rack-cors gem to allow api calls from different domains.

--- estudologia/config/application.rb
+++ estudologia/config/application.rb
@@ -12,5 +12,11 @@ module Estudologia
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = 'pt-BR'
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete]
+      end
+    end
   end
 end
